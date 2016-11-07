3aa78ef40743d1736867994e8264e57dbc86cdf9
tferenc <tamasferenc90@gmail.com>
2016-11-07 14:20:17 +0100

    tamasferenc

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,23 +1,24 @@
-#def fizzbuzz(number):
-for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
-        i = "FizzBuzz"
-    elif i % 3 == 0:
-        i = "Fizz"
-    elif i % 5 == 0:
-        i = "Buzz"
-    else:
-        i = i
-    print(i)
-    #return
+def fizzbuzz(number):
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            i = "FizzBuzz"
+        elif i % 3 == 0:
+            i = "Fizz"
+        elif i % 5 == 0:
+            i = "Buzz"
+        else:
+            i = i
+        print(i)
+    return
 
 
 def main():
     for i in range(1,101):
-        print(i)
-    return
+        fizzbuzz(number)
+        break
+
+number = range(1, 101)
 
 if __name__ == '__main__':
     main()
-
-#print(fizzbuzz(12))   
+   
