d793dede8182052f4b0d0ead48753f0c2183ff14
Trixi <bozso.beatrix@gmail.com>
2016-11-07 17:28:29 +0100

    trixi

--- years/years_module.py
+++ years/years_module.py
@@ -1,11 +1,25 @@
 import datetime
 
 
+
 def years(age):
-    return
+    difference = 100 - int(age)
+    this_year = datetime.date.today()
+    years100 = int(this_year.year) + difference
+  
+
+    return years100
 
 
 def main():
+    
+    name = input("What's your name?: ")
+    age = input("How old are you?: ")
+    
+    years(age)
+
+    print("Hello", name, "you will be 100 years old in the year: ", years(age))
+        
     return
 
 
