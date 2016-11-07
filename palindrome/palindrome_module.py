11e8ddb76880c2bb292ed8863bfeb20030b182e9
Trixi <bozso.beatrix@gmail.com>
2016-11-07 17:07:53 +0100

    trixi

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,24 +1,27 @@
 def palindrome(str):
+    str = str.lower()
+    str = "".join(str.split())
+    
     forward_list = []
     backward_list = []
     
     
     forward_list = list(str)
     backward_list = list(reversed(forward_list))
-    if forward_list == backward_list:
-        print(True)
-    else:
-        print(False)
     
-   
-    return
+    if forward_list == backward_list:
+        return True
+    else:
+        return False 
 
 
 def main():
-    string = input("Write me a string!: ").lower()
-    string = "".join(string.split())
-    palindrome(string)
-    return
+    
+    str = input("What's your string?: ")
+    if palindrome(str):
+        print("This is a palindrome")
+    else:
+        print("No palindrome")
 
 
 if __name__ == '__main__':
