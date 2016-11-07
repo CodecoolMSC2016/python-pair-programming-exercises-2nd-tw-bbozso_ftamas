ccff5871dc644f2b6ef3b41c30f5713c53ea2d54
Trixi <bozso.beatrix@gmail.com>
2016-11-07 15:01:32 +0100

    trixi

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,10 +1,26 @@
 def palindrome(str):
+    forward_list = []
+    backward_list = []
+    
+    
+    forward_list = list(str)
+    backward_list = list(reversed(forward_list))
+    print(forward_list)
+    if forward_list == backward_list:
+        print(True)
+    else:
+        print(False)
+    
+   
     return
 
 
 def main():
+    string = input("Write me a string!: ").lower()
+    palindrome(string)
     return
 
 
 if __name__ == '__main__':
     main()
+
