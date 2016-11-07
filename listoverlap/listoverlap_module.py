c1c10ebde05993a15d6e278835aadf6a35e6e39f
Trixi <bozso.beatrix@gmail.com>
2016-11-07 16:23:49 +0100

    trixi

--- listoverlap/listoverlap_module.py
+++ listoverlap/listoverlap_module.py
@@ -3,15 +3,18 @@ b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
 
 def listoverlap(list1, list2):
+    
     c = []
     for i in a and b:  
         if i in a and i in b:
             c.append(i)
     print(c)
-    return
+    
+    return c
 
 
 def main():
+    
     listoverlap(a, b)
     return
 
